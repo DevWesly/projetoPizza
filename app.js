c5f2bdaf8277b6b652bf98b627b0aeb1c481dcc5
DevWesly <dev.lucaswesley@gmail.com>
2022-07-20 16:52:26 -0300

    rganizando o MVC

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+
+//importando o express
 const express = require('express');
+
+//criando a funcao app do express.
 const app = express();
+
+//importando PizzasRouter.
+const PizzasRouter = require("./routes/PizzasRouter");
+
+//criando a rota '/' no servidor
 app.get('/',(req,res)=>{
-    res.send("ola fizitante")
+    res.send("ola Vizitante")
 });
 
+
+//Utilizando o PizzasRouter como roteador da aplicação
+app.use('/',PizzasRouter);
+
+//criando um endereços para rodar a aplicação
 app.listen(3001,()=>{
     console.log("App rodando na porta 3001")
 });
---
+++ routes/PizzasRouter.js
@@ -0,0 +1,9 @@
+
+//importando o express
+const express = require('express');
+
+//criando um roteador 
+const router = express.Router();
+
+//exportando 
+module.exports = router;
