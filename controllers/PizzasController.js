2ff44abf11afd19ea5da6d4e230cde357f0a0a8e
DevWesly <dev.lucaswesley@gmail.com>
2022-08-03 14:41:54 -0300

    criando uma função para se adicionar uma nova pizza menos imagem

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,4 +1,8 @@
-const pizzas = require("../database/Pizzas.json");
+let pizzas = require("../database/Pizzas.json");
+
+const fs = require('fs');
+
+const path = require('path');
 
 module.exports = {
     listar: (req, res) => {
@@ -11,22 +15,30 @@ module.exports = {
     mostrar: (req, res) => {
         //capturar id 
         let id = req.params.id;
-        let idProxima ;
-        let idAnterior ;
+        let idProxima;
+        let idAnterior;
         let posicao = pizzas.findIndex(p => p.id == id);
         let pizza = pizzas[posicao];
 
-        if(posicao == pizzas.length -1){
+        if (posicao == pizzas.length - 1) {
             idProxima = pizzas[0].id
-        }else{idProxima = pizzas[posicao + 1].id};
+        } else {
+            idProxima = pizzas[posicao + 1].id
+        };
 
-        if(posicao == 0 ){
-            idAnterior = pizzas[pizzas.length-1].id;
-        }else{idAnterior = pizzas[posicao - 1].id};
+        if (posicao == 0) {
+            idAnterior = pizzas[pizzas.length - 1].id;
+        } else {
+            idAnterior = pizzas[posicao - 1].id
+        };
 
         console.log(posicao);
 
-        res.render('pizza.ejs',{pizza,idAnterior,idProxima});
+        res.render('pizza.ejs', {
+            pizza,
+            idAnterior,
+            idProxima
+        });
     },
     buscar: (req, res) => {
         let trechoBuscado = req.query.q;
@@ -40,7 +52,8 @@ module.exports = {
             //lembrar de pedir ajuda para prof explicar direitinho pq não entendi muito bem
             // let resultado = pizzas.filter(pizza => pizza.nome.toUpperCase().includes(trechoBuscado.toUpperCase()));
             res.render('pizzas.ejs', {
-                pizzas: resultado, busca: trechoBuscado
+                pizzas: resultado,
+                busca: trechoBuscado
             });
 
 
@@ -48,11 +61,28 @@ module.exports = {
 
     },
 
-    buscaVazia: (req, res) => {
-        res.send('ja farei');
-    },
-
     teste: (req, res) => {
         res.send('Pagina de testes');
+    },
+
+    criarPizza: (req, res) => {
+        res.render('criarPizza.ejs');
+    },
+
+    guardarPizza: (req, res) => {
+        let pizza = {
+            "id": pizzas[pizzas.length - 1].id + 1,
+            "nome": req.body.nomePizza,
+            "ingredientes": req.body.ingredientes.split(","),
+            "preco": req.body.preco,
+            "img": '/img/' + req.body.img,
+            "destaque": false
+        };
+        pizzas.push(pizza);
+
+        fs.writeFileSync(path.join(__dirname, '../database/Pizzas.json'), JSON.stringify(pizzas, null, 4));
+        res.redirect('/');
+
+
     }
 };
